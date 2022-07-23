5735ebe429db55827091e57cf54e0433c5d9dfeb
Adarsh Shukla <shuklaadarsh1932@gmail.com>
2022-07-23 09:51:24 +0530

    Initial Commit

---
+++ ProfileViewAnaimation/ProfileViewAnaimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileViewAnaimationApp.swift
+//  ProfileViewAnaimation
+//
+//  Created by Adarsh Shukla on 23/07/22.
+//
+
+import SwiftUI
+
+@main
+struct ProfileViewAnaimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
